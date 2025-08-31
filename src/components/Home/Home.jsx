fe7d551887c9865cd3570cd98098381582652574
Daniela Elizondo <dani.elizondo.munoz@gmail.com>
2025-08-30 23:14:03 -0400

    Add new CardPizza with Pizza2.js info

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,20 @@
 import './Home.css';
 import Header from "./components_home/Header";
 import CardPizza from "./components_home/CardPizza";
+import {pizzasData} from './Pizzas2';
 
 const Home =()=> {
     return (
         <>
             <Header />
+            <h1 className="h3 fw-bold text-dark">🍕Nuestras Pizzas🍕</h1>
             <div className="container mt-4">
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                    <CardPizza
-                        name="Napolitana"
-                        price={5950}
-                        ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-                        img="pizza1"
-                    />
-                    <CardPizza
-                        name="Española"
-                        price={6950}
-                        ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-                        img="pizza2"
-                    />
-                    <CardPizza
-                        name="Pepperoni"
-                        price={6950}
-                        ingredients={["mozzarella", "pepperoni", "orégano"]}
-                        img="pizza3"
-                    />
+                <div className="row">
+                    {pizzasData.map((pizza) => (
+                        <div className="col-md-4 mb-4" key={pizza.id}>
+                            <CardPizza pizza={pizza} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
