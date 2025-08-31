9e749666ee4fc010fbd454021cca3d6f6f3caa1c
Daniela Elizondo <dani.elizondo.munoz@gmail.com>
2025-08-30 23:36:42 -0400

    correccion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Cart from './components/Cart/Cart';
 import Footer from "./components/Footer/Footer";
 
 
